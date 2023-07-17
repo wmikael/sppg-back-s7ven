8b10713e6b242f90858a65194f4c2f768e4c0628
Mikael Barros <mikaelhjfb@gmail.com>
2023-07-17 18:56:29 -0300

    fix: ajusta configuracao de CORS

--- src/main/java/br/ufma/sppg/config/CorsConfig.java
+++ src/main/java/br/ufma/sppg/config/CorsConfig.java
@@ -9,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Mapeia todas as rotas
-                .allowedOrigins("*") // Permite todas as origens
+        registry.addMapping("/api/**") // Mapeia todas as rotas
+                .allowedOrigins("http://localhost:5173") // Permite todas as origens
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
                 .allowCredentials(true) // Permite o envio de cookies
                 .maxAge(3600); // Tempo máximo de cache das configurações CORS (em segundos)
